25b719eacbeaf3d8f0c3229d6881f2dd857e2e5f
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-09-08 08:08:13 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+//console.log("teste está funcionando")
+
+console.log("inicio...");
+setTimeout(()=>{
+    console.log("Esperou 2 segundos...")
+}, 2000);
+console.log("Fim...")
+
+const somar = require('./somar');
+console.log(somar(5,3));
